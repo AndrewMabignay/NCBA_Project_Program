a205648ca9ac788046fba5fa4b4d57063c3c70b2
Andrew <andrewmabignay49@gmail.com>
2025-03-22 10:45:09 +0800

    13. IN PROGRESS : CONTACT US & Testing Google Map API

--- app/src/main/java/com/example/ncba_project_program/ContactUsFragment.java
+++ app/src/main/java/com/example/ncba_project_program/ContactUsFragment.java
@@ -1,102 +1,111 @@
 package com.example.ncba_project_program;
 
+import android.graphics.Outline;
+
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.content.Context;
+
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewOutlineProvider;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import androidx.fragment.app.Fragment;
 
 public class ContactUsFragment extends Fragment {
+    private ImageView ncbaCubao, ncbaTaytay, ncbaFairview;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.contactus_main_fragment, container, false);
         AddElement(view);
-        AddInteraction();
+        AddInteraction(savedInstanceState);
         return view;
     }
 
+    private WebView webView;
+    private WebSettings webSettings;
     private void AddElement(View view) {
-
-    }
-
-    private void AddInteraction() {
-
-    }
-}
-
-/*
-* package com.example.ncba_project_program;
-
-import android.os.Bundle;
-import android.os.Handler;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
-
-import androidx.fragment.app.Fragment;
-
-public class AcademicFragment extends Fragment {
-    private ImageView academicImages;
-    private int[] academicImageList = {R.drawable.avr_first_image, R.drawable.gym_second_image};
-    private int currentIndex = 0;
-    private Handler handler = new Handler();
-    private Runnable runnable;
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.academic_main_fragment, container, false);
-        AddElement(view);
-        AddInteraction();
-        return view;
-    }
-
-    private void AddElement(View view) {
-        academicImages = view.findViewById(R.id.AcademicPictures);
-    }
-
-    private void AddInteraction() {
-        startImageTransition();
-    }
-
-    private void startImageTransition() {
-        runnable = new Runnable() {
+        // 1. RADIUS FIELDS
+        ncbaCubao = view.findViewById(R.id.ncbaCubao);
+        ncbaCubao.setClipToOutline(true);
+        ncbaCubao.setOutlineProvider(new ViewOutlineProvider() {
             @Override
-            public void run() {
-                // Slide out animation (current image)
-                Animation slideOut = AnimationUtils.loadAnimation(getActivity().getApplicationContext(), R.anim.slide_out);
-                academicImages.startAnimation(slideOut);
-
-                // Palitan ang image pagkatapos ng animation
-                slideOut.setAnimationListener(new Animation.AnimationListener() {
-                    @Override
-                    public void onAnimationStart(Animation animation) {}
-
-                    @Override
-                    public void onAnimationEnd(Animation animation) {
-                        // Change to the next image
-                        currentIndex = (currentIndex + 1) % academicImageList.length;
-                        academicImages.setImageResource(academicImageList[currentIndex]);
-
-                        // Apply slide-in animation for the new image
-                        Animation slideIn = AnimationUtils.loadAnimation(getActivity().getApplicationContext(), R.anim.slide_in);
-                        academicImages.startAnimation(slideIn);
-                    }
-
-                    @Override
-                    public void onAnimationRepeat(Animation animation) {}
-                });
-
-                // Loop every 3 seconds
-                handler.postDelayed(this, 3000);
+            public void getOutline(View view, Outline outline) {
+                int radius = 28;
+                outline.setRoundRect(0, 0, view.getWidth(), view.getHeight(), radius);
             }
-        };
-        handler.post(runnable);
+        });
+
+        ncbaTaytay = view.findViewById(R.id.ncbaTaytay);
+        ncbaTaytay.setClipToOutline(true);
+        ncbaTaytay.setOutlineProvider(new ViewOutlineProvider() {
+            @Override
+            public void getOutline(View view, Outline outline) {
+                int radius = 28;
+                outline.setRoundRect(0, 0, view.getWidth(), view.getHeight(), radius);
+            }
+        });
+
+        ncbaFairview = view.findViewById(R.id.ncbaFairview);
+        ncbaFairview.setClipToOutline(true);
+        ncbaFairview.setOutlineProvider(new ViewOutlineProvider() {
+            @Override
+            public void getOutline(View view, Outline outline) {
+                int radius = 28;
+                outline.setRoundRect(0, 0, view.getWidth(), view.getHeight(), radius);
+            }
+        });
+
+        webView = view.findViewById(R.id.webView);
+        webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+
+//        // Check internet connection
+//        if (isInternetAvailable()) {
+//            webView.setVisibility(View.VISIBLE); // Show WebView if internet is available
+//            webView.loadData(
+//                    "<iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3860.4841975731683!2d121.06796262756052!3d14.628434175438064!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b78fe2ccb011%3A0x7375abf629d6685a!2sNational%20College%20of%20Business%20and%20Arts%20(NCBA)!5e0!3m2!1sen!2sph!4v1742610948433!5m2!1sen!2sph' width='100%' height='100%' style='border:0;' allowfullscreen='' loading='lazy'></iframe>",
+//                    "text/html",
+//                    "UTF-8"
+//            );
+//        } else {
+//            webView.setVisibility(View.GONE); // Hide WebView if no internet
+//        }
+
+
+
+
+
+
+    }
+//
+//    // Method to check internet connection
+//    private boolean isInternetAvailable() {
+//        ConnectivityManager cm = (ConnectivityManager) requireContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+//        if (cm != null) {
+//            NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+//            return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+//        }
+//        return false;
+//    }
+
+
+
+
+
+
+    private void AddInteraction(Bundle savedInstanceState) {
+
     }
 }
-
-* */
