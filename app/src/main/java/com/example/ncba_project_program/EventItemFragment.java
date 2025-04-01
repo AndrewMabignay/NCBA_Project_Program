1f7c9e5231f2fca80b14126166391dd5d5dba0ac
Andrew <andrewmabignay49@gmail.com>
2025-04-01 21:57:56 +0800

    Modifying Events

--- app/src/main/java/com/example/ncba_project_program/EventItemFragment.java
+++ app/src/main/java/com/example/ncba_project_program/EventItemFragment.java
@@ -1,18 +1,28 @@
 package com.example.ncba_project_program;
 
+import android.app.AlertDialog;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.StyleSpan;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 public class EventItemFragment extends Fragment {
     private static final String ARG_TITLE = "title";
     private static final String ARG_DESCRIPTION = "description";
     private static final String ARG_IMAGE_RES_ID = "imageResId";
+    private LinearLayout readMore;
 
     public static EventItemFragment newInstance(Event event) {
         EventItemFragment fragment = new EventItemFragment();
@@ -36,11 +46,276 @@ public class EventItemFragment extends Fragment {
         TextView titleText = view.findViewById(R.id.textTitle);
         TextView descriptionText = view.findViewById(R.id.textDescription);
         ImageView eventImage = view.findViewById(R.id.imageView);
+        readMore = view.findViewById(R.id.ReadMore);
+
 
         if (getArguments() != null) {
             titleText.setText(getArguments().getString(ARG_TITLE, "No Title"));
             descriptionText.setText(getArguments().getString(ARG_DESCRIPTION, "No Description"));
             eventImage.setImageResource(getArguments().getInt(ARG_IMAGE_RES_ID, R.drawable.bg_dialog));
+
+            readMore.setOnClickListener(e -> {
+                String eventTitle = titleText.getText().toString();
+                showEventDialog(eventTitle);
+            });
         }
     }
+
+    private void showEventDialog(String eventTitle) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.activity_main_event_item_sub_fragment_dialog, null);
+
+        TextView titleViewDialog = dialogView.findViewById(R.id.textTitle);
+        TextView dateViewDialog = dialogView.findViewById(R.id.textDate);
+        TextView descriptionViewDialog = dialogView.findViewById(R.id.textDescription);
+        ImageView imageViewDialog = dialogView.findViewById(R.id.imageViewArticle);
+        TextView textArticle = dialogView.findViewById(R.id.textArticleDescription);
+
+        String date;
+        String description;
+        int imageResId;
+        String article;
+
+        SpannableString spannableArticle;
+        int start;
+
+        switch (eventTitle) {
+            //  EVENT 1
+            case "Life Saving Blood Donation":
+                date = "March 10, 2025";
+                description = "Community Outreach Program, Events, Fairview";
+                imageResId = R.drawable.blood_donation_event_one;
+                article = getString(R.string.article_text_event_one);
+
+                spannableArticle = new SpannableString(article);
+
+                applyHashtagColor(spannableArticle, "#BloodDonation", R.color.colorBlue);
+                applyHashtagColor(spannableArticle, "#SaveLives", R.color.colorBlue);
+
+                textArticle.setText(spannableArticle);
+                break;
+
+            //  EVENT 2
+            case "Bringing Care to the Community!":
+                date = "March 10, 2025";
+                description = "Community Outreach Program, Events, Fairview";
+                imageResId = R.drawable.care_community_event_two;
+                article = getString(R.string.article_text_event_two);
+
+                spannableArticle = new SpannableString(article);
+
+                applyHashtagColor(spannableArticle, "#NCBA58YearsStrong", R.color.colorBlue);
+                applyHashtagColor(spannableArticle, "#DriveForChange", R.color.colorBlue);
+                applyHashtagColor(spannableArticle, "#NCBACares", R.color.colorBlue);
+                applyHashtagColor(spannableArticle, "#JCIQuezonCityCapitol", R.color.colorBlue);
+                applyHashtagColor(spannableArticle, "#MedicalMission2025", R.color.colorBlue);
+
+                textArticle.setText(spannableArticle);
+                break;
+
+            //  EVENT 3
+            case "NCBA Taytay Open House: A Glimpse Inside":
+                date = "March 5, 2025";
+                description = "58th Foundation Day, Events, Taytay";
+                imageResId = R.drawable.glimpse_inside_event_three;
+                article = getString(R.string.article_text_event_three);
+                textArticle.setText(article);
+                break;
+
+            //  EVENT 4
+            case "NCBA Taytay Rocks: Battle of the Bands 2025":
+                date = "March 5, 2025";
+                description = "58th Foundation Day, Events, Taytay";
+                imageResId = R.drawable.battle_of_the_bands_event_four;
+                article = getString(R.string.article_text_event_four);
+                textArticle.setText(article);
+                break;
+
+            //  EVENT 5
+            case "NCBA COP Launched a Adopted Community Program":
+                date = "October 18, 2024";
+                description = "Community Outreach Program, Events, Taytay";
+                imageResId = R.drawable.adopted_community_event_five;
+                article = getString(R.string.article_text_event_five);
+                textArticle.setText(article);
+                break;
+
+            //  EVENT 6
+            case "BSHM Second Year Students (Bread and Pastry)":
+                date = "October 14, 2024";
+                description = "BSHM, College, Events";
+                imageResId = R.drawable.bread_pastry_event_six;
+                article = getString(R.string.article_text_event_six);
+                textArticle.setText(article);
+                break;
+
+            //  EVENT 7
+            case "NCBA Job Fair 2024":
+                date = "May 8, 2024";
+                description = "College, Cubao, Fairview, Taytay, Upcoming Events";
+                imageResId = R.drawable.ncba_job_fair_event_seven;
+                article = getString(R.string.article_text_event_seven);
+
+                spannableArticle = new SpannableString(article);
+                applyBoldText(spannableArticle, "Heads Up, NCBA College Seniors!");
+                applyBoldText(spannableArticle, "May 10, 2024 (Friday)");
+                applyBoldText(spannableArticle, "9:00 am – 4:00 pm");
+                applyBoldText(spannableArticle, "Founders Hall, NCBA Fairview");
+
+                textArticle.setText(spannableArticle);
+                break;
+
+            //  EVENT 8
+            case "The Rise of Digitalization":
+                date = "May 7, 2024";
+                description = "BSBA, College, Events";
+                imageResId = R.drawable.rise_digitalization_event_eight;
+                article = getString(R.string.article_text_event_eight);
+
+                spannableArticle = new SpannableString(article);
+                applyBoldText(spannableArticle, "The Rise of Digitalization: Impact of Digital Services in the Financial Sector");
+
+                textArticle.setText(spannableArticle);
+                break;
+
+            //  EVENT 9
+            case "Ethical Hacking Seminar":
+                date = "April 22, 2024";
+                description = "BSCS, College, Taytay";
+                imageResId = R.drawable.ethical_hacking_event_nine;
+                article = getString(R.string.article_text_event_nine);
+                textArticle.setText(article);
+                break;
+
+            //  EVENT 10
+            case "Filipino Food Month":
+                date = "April 22, 2024";
+                description = "BSHM, College, Events";
+                imageResId = R.drawable.filipino_food_month_event_ten;
+                article = getString(R.string.article_text_event_ten);
+
+                spannableArticle = new SpannableString(article);
+
+                applyHashtagColor(spannableArticle, "#NCBA", R.color.colorBlue);
+                applyHashtagColor(spannableArticle, "#wildcats", R.color.colorBlue);
+                applyHashtagColor(spannableArticle, "#HMDepartment", R.color.colorBlue);
+
+                textArticle.setText(spannableArticle);
+                break;
+
+            //  EVENT 11
+            case "SSS and PAG-IBIG Seminar":
+                date = "April 15, 2024";
+                description = "BSBA, College, Events, Taytay";
+                imageResId = R.drawable.sss_pagibig_event_eleven;
+                article = getString(R.string.article_text_event_eleven);
+
+                textArticle.setText(article);
+                break;
+
+            //  EVENT 12
+            case "SSS eWheels on Campus":
+                date = "March 24, 2024";
+                description = "Events, Fairview, General";
+                imageResId = R.drawable.sss_ewheel_event_twelve;
+                article = getString(R.string.article_text_event_twelve);
+
+                spannableArticle = new SpannableString(article);
+                applyBoldText(spannableArticle, "March 25, 9:00–5:00pm");
+                applyBoldText(spannableArticle, "EXCLUSIVE for NCBA Fairview students only!");
+
+                textArticle.setText(spannableArticle);
+                break;
+
+            //  EVENT 13
+            case "Gifted Children Awareness Film Showing":
+                date = "March 13, 2024";
+                description = "College, Cubao, Events";
+                imageResId = R.drawable.children_awareness_event_thirteen;
+                article = getString(R.string.article_text_event_thirteen);
+
+                textArticle.setText(article);
+                break;
+
+            //  EVENT 14
+            case "PACUCOA Accreditors Visit NCBA Cubao":
+                date = "Feb 29, 2024";
+                description = "College, Cubao, Events";
+                imageResId = R.drawable.pacucoa_event_fourteen;
+                article = getString(R.string.article_text_event_fourteen);
+
+                textArticle.setText(article);
+                break;
+
+            //  EVENT 15
+            case "COP’s Livelihood Training: Make Dishwashing and Hand Soap":
+                date = "November 28, 2022";
+                description = "Events";
+                imageResId = R.drawable.soup_event_fifteen;
+                article = getString(R.string.article_text_event_fifteen);
+
+                textArticle.setText(article);
+                break;
+
+            default:
+                date = "Unknown Date";
+                description = "No description available.";
+                imageResId = R.drawable.redhorse; // Use a default image in case
+                article = "No article available.";
+                break;
+        }
+
+        titleViewDialog.setText(eventTitle);
+        dateViewDialog.setText(date);
+        descriptionViewDialog.setText(description);
+        imageViewDialog.setImageResource(imageResId);
+
+        builder.setView(dialogView);
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+        if (alertDialog.getWindow() != null) {
+            alertDialog.getWindow().setBackgroundDrawableResource(R.drawable.rounded_dialog); // ✅ Apply Rounded Corners
+            alertDialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation; // (Optional) Animation
+        }
+
+        LinearLayout closeButton = dialogView.findViewById(R.id.CloseButton);
+        closeButton.setOnClickListener(v -> {
+            alertDialog.dismiss();
+        });
+
+        // ✅ Prevent dismissing when clicking outside
+        alertDialog.setCancelable(false);
+        alertDialog.setCanceledOnTouchOutside(false);
+
+        // ✅ Handle Back Button Press
+        alertDialog.setOnKeyListener((dialog, keyCode, event) -> {
+            if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_UP) {
+                alertDialog.dismiss(); // Isasara ang dialog
+                return true; // Iwasan ang default back button behavior
+            }
+            return false;
+        });
+
+        alertDialog.show();
+    }
+
+    private void applyHashtagColor(SpannableString spannable, String hashtag, int colorResId) {
+        int start = spannable.toString().indexOf(hashtag);
+        if (start != -1) {
+            int end = start + hashtag.length();
+            spannable.setSpan(new ForegroundColorSpan(ContextCompat.getColor(requireContext(), colorResId)),
+                    start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        }
+    }
+
+    private void applyBoldText(SpannableString spannable, String targetText) {
+        int start = spannable.toString().indexOf(targetText);
+        if (start != -1) {
+            int end = start + targetText.length();
+            spannable.setSpan(new StyleSpan(Typeface.BOLD), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        }
+    }
+
 }
--- app/src/main/java/com/example/ncba_project_program/MainFragment.java
+++ app/src/main/java/com/example/ncba_project_program/MainFragment.java
@@ -1,18 +1,24 @@
 package com.example.ncba_project_program;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.Scroller;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
+import android.view.animation.Interpolator;
 
+
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,6 +27,10 @@ public class MainFragment extends Fragment {
 
     private ViewPager2 viewPager2;
     private EventPagerAdapter adapter;
+    private Handler handler;
+    private Runnable autoScrollRunnable;
+    private int currentPage = 0;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -43,14 +53,55 @@ public class MainFragment extends Fragment {
 
         viewPager2 = view.findViewById(R.id.viewPagerHome);
         List<Event> eventList = new ArrayList<>();
-        eventList.add(new Event("Open House 2024", "A chance to explore our campus!", R.drawable.blood_donation_event_one));
-        eventList.add(new Event("IT Seminar", "Learn the latest trends in technology.", R.drawable.blood_donation_event_one));
-        eventList.add(new Event("Sports Fest", "Showcase your skills in various sports!", R.drawable.blood_donation_event_one));
+        eventList.add(new Event("Life Saving Blood Donation", "Community Outreach Program, Events, Fairview", R.drawable.blood_donation_event_one));
+        eventList.add(new Event("Bringing Care to the Community!", "COP, Cubao, Events.", R.drawable.care_community_event_two));
+        eventList.add(new Event("NCBA Taytay Open House: A Glimpse Inside", "58th Foundation Day, Events, Taytay", R.drawable.glimpse_inside_event_three));
+        eventList.add(new Event("NCBA Taytay Rocks: Battle of the Bands 2025", "58th Foundation Day, Events, Taytay", R.drawable.battle_of_the_bands_event_four));
+        eventList.add(new Event("NCBA COP Launched a Adopted Community Program", "COP, Events, Taytay", R.drawable.adopted_community_event_five));
+        eventList.add(new Event("BSHM Second Year Students (Bread and Pastry)", "BSHM, College, Events", R.drawable.bread_pastry_event_six));
+        eventList.add(new Event("NCBA Job Fair 2024", "College, Cubao, Fairview, Taytay, Upcoming Events", R.drawable.ncba_job_fair_event_seven));
+        eventList.add(new Event("The Rise of Digitalization", "BSBA, College, Events\n", R.drawable.rise_digitalization_event_eight));
+        eventList.add(new Event("Ethical Hacking Seminar", "BSCS, College, Taytay\n", R.drawable.ethical_hacking_event_nine));
+        eventList.add(new Event("Filipino Food Month", "BSHM, College, Events\n", R.drawable.filipino_food_month_event_ten));
+        eventList.add(new Event("SSS and PAG-IBIG Seminar", "BSBA, College, Events, Taytay\n", R.drawable.sss_pagibig_event_eleven));
+        eventList.add(new Event("SSS eWheels on Campus", "Events, Fairview, General\n", R.drawable.sss_ewheel_event_twelve));
+        eventList.add(new Event("Gifted Children Awareness Film Showing", "College, Cubao, Events", R.drawable.children_awareness_event_thirteen));
+        eventList.add(new Event("PACUCOA Accreditors Visit NCBA Cubao", "College, Cubao, Events", R.drawable.pacucoa_event_fourteen));
+        eventList.add(new Event("COP’s Livelihood Training: Make Dishwashing and Hand Soap", "Events", R.drawable.soup_event_fifteen));
 
-        // SET ADAPTER
         adapter = new EventPagerAdapter(requireActivity(), eventList);
         viewPager2.setAdapter(adapter);
 
+        viewPager2.setPageTransformer(new ViewPager2.PageTransformer() {
+            @Override
+            public void transformPage(@NonNull View page, float position) {
+                page.setAlpha(1 - Math.abs(position)); // Fade effect
+                page.setTranslationX(-position * page.getWidth() * 0.2f); // Horizontal slide
+            }
+        });
+
+        // Start auto-scroll functionality without using Handler
+        viewPager2.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                int itemCount = viewPager2.getAdapter().getItemCount();
+                if (currentPage >= itemCount) {
+                    currentPage = 0; // Loop back to the first item when reaching the last
+                }
+
+                viewPager2.setCurrentItem(currentPage++, true); // Smooth transition to next page
+                viewPager2.postDelayed(this, 4000); // Delay of 4 seconds for smooth transition
+            }
+        }, 2000);
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        if (handler != null) {
+            handler.removeCallbacks(autoScrollRunnable);
+        }
     }
 
     private Fragment admissionFragment, academicFragment, aboutUsFragment, webMasterFragment, contactUsFragment;
@@ -167,3 +218,22 @@ class Event {
     }
 }
 
+
+
+
+class CustomScroller extends Scroller {
+    private int mDuration;
+
+    public CustomScroller(Context context, Interpolator interpolator) {
+        super(context, interpolator);
+    }
+
+    @Override
+    public void startScroll(int startX, int startY, int dx, int dy, int duration) {
+        super.startScroll(startX, startY, dx, dy, mDuration);
+    }
+
+    public void setScrollDuration(int duration) {
+        this.mDuration = duration;
+    }
+}
